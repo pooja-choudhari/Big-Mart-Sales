f8264f9b3b782b763a36d4bc95f80e0661b67770
Pooja Choudhari <poojachoudhari0792@gmail.com>
2020-06-02 17:29:04 -0500

    Missing value Imputation

--- BigMart-Sales.R
+++ BigMart-Sales.R
@@ -131,3 +131,20 @@ ggplot(train) + geom_violin(aes(Outlet_Size, Item_Outlet_Sales), fill = "lightgr
 plot15 = ggplot(train) + geom_violin(aes(Outlet_Location_Type, Item_Outlet_Sales), fill = "magenta") 
 plot16 = ggplot(train) + geom_violin(aes(Outlet_Type, Item_Outlet_Sales), fill = "magenta") 
 plot_grid(plot15, plot16, ncol = 1)
+
+#--- Missing Value Imputation-----
+colSums(is.na(combi))
+
+missing_index = which(is.na(combi$Item_Weight)) 
+for(i in missing_index){   
+  item = combi$Item_Identifier[i]  
+  combi$Item_Weight[i] = mean(combi$Item_Weight[combi$Item_Identifier == item], na.rm = T) 
+  }
+
+#Replacing 0’s in Item_Visibility variable
+zero_index = which(combi$Item_Visibility == 0) 
+for(i in zero_index){    
+  item = combi$Item_Identifier[i]  
+  combi$Item_Visibility[i] = mean(combi$Item_Visibility[combi$Item_Identifier == item], na.rm = T) 
+  }
+  
